19b05a75b18de5c5890e70d5574dad277e90f580
itnth <it.nth02@gmail.com>
2023-11-23 14:36:57 +0700

    checkin

---
+++ src/common/Enum.ts
@@ -0,0 +1,21 @@
+export const Enumeration = {
+  Priority: {
+    None: 0,
+    High: 1,
+    Medium: 2,
+    Low: 3
+  },
+  Status: {
+    None: 0,
+    Approved: 1,
+    Pendding: 2,
+    Done: 3
+  },
+  Editmode: {
+    None: 0,
+    Add: 1,
+    Edit: 2,
+    Delete: 3,
+    Copy: 4
+  }
+}
--- src/common/axios.ts
+++ src/common/axios.ts
@@ -1,43 +1,50 @@
-import baseAxios from 'axios';
-import moment from 'moment';
+import baseAxios from 'axios'
+import moment from 'moment'
 
 // Create an Axios instance
 const axios = baseAxios.create({
-    baseURL: 'https://localhost:7081/api/', // Replace with your base URL
-});
+  baseURL: 'https://localhost:7081/api/' // Replace with your base URL
+})
 // Add a response interceptor
 axios.interceptors.response.use(
-    (response: any) => {
-        if (response.data && response.data.Success) {
-            response.data = parseDates(response.data);
-        }
-        return response.data;
-    },
-    (error: any) => {
-        // Handle response error
-        return Promise.reject(error);
+  (response: any) => {
+    if (response.data && response.data.Success) {
+      response.data = parseDates(response.data)
     }
-);
-
+    return response.data
+  },
+  (error: any) => {
+    // Handle response error
+    return Promise.reject(error)
+  }
+)
+// Define the regex pattern for the date format
+// const datePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3}|\.\d{2}|\.\d{1}|\.\d{4}|\.\d{5}|\.\d{6}|\.\d{7}|\.\d{8}|\.\d{9})?Z$/;
+// eslint-disable-next-line no-useless-escape
+const datePattern = /^\d{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\d|3[0-1])T(?:[0-1]\d|2[0-3]):[0-5]\d:[0-5]\d(?:\.\d+|)(?:Z|(?:\+|\-)(?:\d{2}):?(?:\d{2}))$/
+// Test function
+const testDateFormat = (input: string) => {
+  return datePattern.test(input)
+}
 // Helper function to parse dates in the response data
-function parseDates(data: { [x: string]: any; hasOwnProperty: (arg0: string) => any; }) {
-    // Recursively traverse the response data to find and parse date strings
-    for (const key in data) {
-        // eslint-disable-next-line no-prototype-builtins
-        if (data.hasOwnProperty(key)) {
-            if (typeof data[key] === 'string') {
-                // Attempt to parse date strings using moment
-                const parsedDate = moment(data[key]);
-                if (parsedDate.isValid()) {
-                    data[key] = parsedDate.toDate();
-                }
-            } else if (typeof data[key] === 'object') {
-                // Recurse into nested objects or arrays
-                data[key] = parseDates(data[key]);
-            }
+function parseDates(data: any) {
+  // Recursively traverse the response data to find and parse date strings
+  for (const key in data) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (data.hasOwnProperty(key)) {
+      if (typeof data[key] === 'string' && testDateFormat(data[key])) {
+        // Attempt to parse date strings using moment
+        const parsedDate = moment(data[key])
+        if (parsedDate.isValid()) {
+          data[key] = parsedDate.toDate()
         }
+      } else if (typeof data[key] === 'object') {
+        // Recurse into nested objects or arrays
+        data[key] = parseDates(data[key])
+      }
     }
-    return data;
+  }
+  return data
 }
 
-export default axios;
+export default axios
